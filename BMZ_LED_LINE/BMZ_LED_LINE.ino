a4867661b2fca6ce43df51ced4b2a12fd1798f28
Sergg-BMZ-PC <Sergg@SERGG-BMZ-PC>
2020-10-22 19:38:13 +0300

    upd

--- BMZ_LED_LINE/BMZ_LED_LINE.ino
+++ BMZ_LED_LINE/BMZ_LED_LINE.ino
@@ -11,8 +11,13 @@ int oePin = A2;
   digit in the 7 segment display from 0 to 9
 
 */
-byte segChar[] = {63, 6, 91, 79, 102, 109, 125, 7, 127, 111};
-byte o = 2;
+byte segChar[] = {4, 8, 16, 32,
+                  64, 128, 256, 512,
+                  1024, 2048, 4096, 8192,
+                  16384, 32768, 65536, 131072
+                 };
+int t = 25;
+
 void setup() {
   //set pins to output because they are addressed in the main loop
   pinMode(strobePin, OUTPUT);
@@ -24,21 +29,32 @@ void setup() {
 }
 
 void loop() {
-  for (int j = 0; j < 9; j++) {
-    Serial.println(j);
-    //устанавливаем LOW на latchPin пока не окончена передача байта
-    digitalWrite(strobePin, LOW);
-    shiftOut(dataPin, clockPin, LSBFIRST, j);
-    digitalWrite(strobePin, HIGH);
-    delay(1000);
-  }
-  /*
-    for (int i = 0; i < 10; i++) {
+
+  /* for (int j = 0; j < 99999999; j += 16) {
+     byte qw = 4; // int 4 blue, 8 green
+     Serial.println(j);
+     //устанавливаем LOW на latchPin пока не окончена передача байта
+     digitalWrite(strobePin, LOW);
+     shiftOut(dataPin, clockPin, LSBFIRST, j);
+     digitalWrite(strobePin, HIGH);
+     delay(100);
+    }
+  */
+
+  for (int i = 0; i < 13; i++) {
     //set strobe pin low to begin storing bits
     digitalWrite(strobePin, LOW);
-    shiftOut(dataPin, clockPin, MSBFIRST, segChar[i]);
+    shiftOut(dataPin, clockPin, LSBFIRST, segChar[i]);
     //set strobe pin high to stop storing bits
     digitalWrite(strobePin, HIGH);
-    delay(250);
-  */
+    delay(t);
+  }
+  for (int i = 13; i > 0; i--) {
+    //set strobe pin low to begin storing bits
+    digitalWrite(strobePin, LOW);
+    shiftOut(dataPin, clockPin, LSBFIRST, segChar[i]);
+    //set strobe pin high to stop storing bits
+    digitalWrite(strobePin, HIGH);
+    delay(t);
+  }
 }
